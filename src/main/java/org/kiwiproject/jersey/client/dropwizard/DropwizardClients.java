a296d6abfb451bc074119f55df6fb77cc206acfb
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-12-18 13:28:02 -0500

    Add DropwizardClients utility class
    * Add DropwizardClients, which provides the ability to register a
      Dropwizard JacksonMessageBodyProvider on a JAX-RS Client
    * Because DropwizardClients uses JacksonMessageBodyProvider, which in
      turn extends the Jackson class JacksonJaxbJsonProvider, had to change
      the jackson-jaxrs-json-provider scope from test to provided
    * Was able to remove kiwi exclusion from service-discovery-client
      since there are no more convergence errors
    
    Closes #50

---
+++ src/main/java/org/kiwiproject/jersey/client/dropwizard/DropwizardClients.java
@@ -0,0 +1,48 @@
+package org.kiwiproject.jersey.client.dropwizard;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.jersey.jackson.JacksonMessageBodyProvider;
+import lombok.experimental.UtilityClass;
+
+import javax.ws.rs.client.Client;
+
+/**
+ * Utilities for adding Dropwizard features to JAX-RS Clients.
+ */
+@UtilityClass
+public class DropwizardClients {
+
+    /**
+     * Adds a {@link JacksonMessageBodyProvider} to the given {@link Client}.
+     * <p>
+     * See the note explaining when this is not needed in the description of
+     * {@link #addJacksonMessageBodyProvider(Client, JacksonMessageBodyProvider)}.
+     *
+     * @param client the Client to add the {@link JacksonMessageBodyProvider}
+     * @param mapper the {@link ObjectMapper} that will be supplied to the {@link JacksonMessageBodyProvider}
+     * @return the Client argument, for method chaining
+     * @see #addJacksonMessageBodyProvider(Client, JacksonMessageBodyProvider)
+     */
+    public static Client addJacksonMessageBodyProvider(Client client, ObjectMapper mapper) {
+        return addJacksonMessageBodyProvider(client, new JacksonMessageBodyProvider(mapper));
+    }
+
+    /**
+     * Adds the {@link JacksonMessageBodyProvider} to the given {@link Client}.
+     * <p>
+     * Note that this is not necessary when using {@link DropwizardManagedClientBuilder} to build clients, because
+     * Dropwizard adds {@link JacksonMessageBodyProvider}. It is mainly useful in a Dropwizard application when using
+     * {@link org.kiwiproject.jersey.client.RegistryAwareClientBuilder RegistryAwareClientBuilder} to build clients,
+     * since it is not aware of Dropwizard, and you need to customize JSON serialization via a custom
+     * {@link ObjectMapper}. For example, if you use milliseconds for all timestamps instead of Jackson's default
+     * serialization of Java date/time objects like {@link java.time.ZonedDateTime}, you need to configure the
+     * ObjectMapper to read and write timestamps as milliseconds.
+     *
+     * @param client   the Client to add the {@link JacksonMessageBodyProvider}
+     * @param provider the {@link JacksonMessageBodyProvider} to add
+     * @return the Client argument, for method chaining
+     */
+    public static Client addJacksonMessageBodyProvider(Client client, JacksonMessageBodyProvider provider) {
+        return client.register(provider);
+    }
+}
