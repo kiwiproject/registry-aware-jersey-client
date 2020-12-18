1436c424107d8ca5f8c135a1f445a99661650f61
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-12-18 17:07:54 -0500

    Add ability to set timeouts from ServiceIdentifier in ClientBuilder
    * Add timeoutsFrom(ServiceIdentifier) to ClientBuilder
    * Implement timeoutsFrom in RegistryAwareClientBuilder
    
    Closes #56

--- src/main/java/org/kiwiproject/jersey/client/ClientBuilder.java
+++ src/main/java/org/kiwiproject/jersey/client/ClientBuilder.java
@@ -12,19 +12,19 @@ import java.util.function.Supplier;
  * Configure and create {@link RegistryAwareClient} instances using a fluent builder interface. Use {@link ClientBuilders}
  * to get a builder instance.
  * <p>
- * Inspired by {@link javax.ws.rs.client.ClientBuilder}
+ * Inspired by {@link javax.ws.rs.client.ClientBuilder}.
  */
 public interface ClientBuilder {
 
     /**
-     * Enables the multipart feature on the client
+     * Enables the multipart feature on the client.
      *
      * @return this builder
      */
     ClientBuilder multipart();
 
     /**
-     * Sets the connect timeout for the client
+     * Sets the connect timeout for the client.
      *
      * @param millis length of timeout in millis
      * @return this builder
@@ -32,7 +32,7 @@ public interface ClientBuilder {
     ClientBuilder connectTimeout(int millis);
 
     /**
-     * Sets the connect timeout for the client
+     * Sets the connect timeout for the client.
      *
      * @param millis length of timeout in millis
      * @return this builder
@@ -40,7 +40,7 @@ public interface ClientBuilder {
     ClientBuilder connectTimeout(long millis);
 
     /**
-     * Sets the read timeout for the client
+     * Sets the read timeout for the client.
      *
      * @param millis length of timeout in millis
      * @return this builder
@@ -48,7 +48,7 @@ public interface ClientBuilder {
     ClientBuilder readTimeout(int millis);
 
     /**
-     * Sets the read timeout for the client
+     * Sets the read timeout for the client.
      *
      * @param millis length of timeout in millis
      * @return this builder
@@ -56,7 +56,22 @@ public interface ClientBuilder {
     ClientBuilder readTimeout(long millis);
 
     /**
-     * Sets the hostname verifier for the client
+     * Sets the connect and read timeouts for the client from a {@link ServiceIdentifier}. This is most useful
+     * when you already have a {@link ServiceIdentifier} for a specific service, and you want to create a new
+     * {@link RegistryAwareClient} for use making requests to that service. This in no way precludes using the
+     * client to connect to any other service, but is a convenience when you are using the "bulkhead" pattern.
+     * <p>
+     * See Michael Nygard's excellent book <a href="https://pragprog.com/titles/mnee2/release-it-second-edition/">Release It!</a>
+     * for information on stability patterns including bulkheads. Microsoft also has a good description in their
+     * <a href="https://docs.microsoft.com/en-us/azure/architecture/patterns/bulkhead">Azure design patterns</a>.
+     *
+     * @param serviceIdentifier defines the read and connect timeouts to set on the client
+     * @return this builder
+     */
+    ClientBuilder timeoutsFrom(ServiceIdentifier serviceIdentifier);
+
+    /**
+     * Sets the hostname verifier for the client.
      *
      * @param hostnameVerifier The {@link HostnameVerifier} to use
      * @return this builder
@@ -64,7 +79,7 @@ public interface ClientBuilder {
     ClientBuilder hostnameVerifier(HostnameVerifier hostnameVerifier);
 
     /**
-     * Sets the SSL context for the client
+     * Sets the SSL context for the client.
      *
      * @param sslContext The {@link SSLContext} to use
      * @return this builder
@@ -72,7 +87,7 @@ public interface ClientBuilder {
     ClientBuilder sslContext(SSLContext sslContext);
 
     /**
-     * Provides a {@link TlsConfigProvider} to use to provide TLS configuration
+     * Provides a {@link TlsConfigProvider} to use to provide TLS configuration.
      *
      * @param tlsConfigProvider The config provider to provide TLS settings
      * @return this builder
@@ -80,7 +95,7 @@ public interface ClientBuilder {
     ClientBuilder tlsConfigProvider(TlsConfigProvider tlsConfigProvider);
 
     /**
-     * Provides the {@link RegistryClient} to use to find services
+     * Provides the {@link RegistryClient} to use to find services.
      *
      * @param registryClient The {@link RegistryClient} responsible for looking up services to connect to
      * @return this builder
@@ -97,7 +112,7 @@ public interface ClientBuilder {
     ClientBuilder headersSupplier(Supplier<Map<String, Object>> headersSupplier);
 
     /**
-     * Builds the {@link RegistryAwareClient} with the configured options
+     * Builds the {@link RegistryAwareClient} with the configured options.
      *
      * @return a {@link RegistryAwareClient}
      */
